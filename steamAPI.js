31311a1fbc7116470c14a3b5866ad52084daa9f9
Paul Lewis <paul.lewis@complex.com>
2014-03-12 16:58:53 -0400

    Turning request into its own function

--- steamAPI.js
+++ steamAPI.js
@@ -10,24 +10,83 @@ function steamAPI(key) {
     this.endpointDomain = 'http://api.steampowered.com';
 }
 
+steamAPI.request = function(options) {
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          obj = JSON.parse(body);
+          console.log(obj);
+        }
+    });
+}
+
 // ISteamUser
 steamAPI.prototype.GetFriendList = function(steamid) {
 
+    var options = {
+        url: this.endpointDomain + '/ISteamUser/GetPlayerSummaries/v0002/',
+        qs: {
+            key: this.key,
+            steamids: steamids
+        }
+    }
+
+    steamAPI.request(options)
+
 }
 
-steamAPI.prototype.GetPlayerBans = function(steamid) {
+steamAPI.prototype.GetPlayerBans = function(steamids) {
+
+    var options = {
+        url: this.endpointDomain + '/ISteamUser/GetPlayerBans/v0001/',
+        qs: {
+            key: this.key,
+            steamids: steamids
+        }
+    }
+
+    steamAPI.request(options)
 
 }
 
-steamAPI.prototype.GetPlayerSummaries = function(steamid) {
+steamAPI.prototype.GetPlayerSummaries = function(steamids) {
+
+    var options = {
+        url: this.endpointDomain + '/ISteamUser/GetPlayerSummaries/v0002/',
+        qs: {
+            key: this.key,
+            steamids: steamids
+        }
+    }
+
+    steamAPI.request(options)
 
 }
 
 steamAPI.prototype.GetUserGroupList = function(steamid) {
 
+    var options = {
+        url: this.endpointDomain + '/ISteamUser/GetUserGroupList/v0001/',
+        qs: {
+            key: this.key,
+            steamid: steamid
+        }
+    }
+
+    steamAPI.request(options)
+
 }
 
 steamAPI.prototype.resolveVanity = function(name) {
+
+    var options = {
+        url: this.endpointDomain + '/ISteamUser/ResolveVanityURL/v0001/',
+        qs: {
+            key: this.key,
+            vanityurl: name
+        }
+    }
+
+    steamAPI.request(options)
 
 }
 
--- test.js
+++ test.js
@@ -2,5 +2,14 @@ var api = require('./steamAPI');
 
 var steamScraper = new api.connection;
 
-steamScraper.getUser('76561197963635996');
-steamScraper.getOwnedGames('76561197963635996');
+steamScraper.resolveVanity('PieKingOne');
+steamScraper.resolveVanity('aaron123');
+
+steamScraper.GetPlayerSummaries(76561197963635996);
+steamScraper.GetPlayerSummaries(76561197969671378);
+
+steamScraper.GetPlayerBans(76561197963635996);
+steamScraper.GetPlayerBans(76561197969671378);
+
+steamScraper.GetUserGroupList(76561197963635996);
+steamScraper.GetUserGroupList(76561197969671378);
